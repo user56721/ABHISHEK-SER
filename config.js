977ea6c87197d59567bd0bcde2920eaa7f242543
user56721 <137920127+user56721@users.noreply.github.com>
2023-09-28 15:26:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['919074692450', 'ABHISHEK-SURESH', true],
+  ['919995219348', 'STARK_BOT', true],
   [''],
   [''] 
 ] //Number of owners
@@ -49,9 +49,9 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁'
 global.premium = 'true'
-global.packname = '𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝙴𝚁🎯𝙰𝙱𝙷𝙸𝚂𝙷𝙴𝙺-𝚂𝚄𝚁𝙴𝚂𝙷☘️' 
-global.author = 'ABHISHEK SURESH' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/abhishek_ser\n' 
+global.packname = 'ＳＴＡＲＫ_ＢＯＴ❤🤍' 
+global.author = '​🇸​​🇹​​🇦​​🇷​​🇰​_​🇧​​🇴​​🇹​' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/cristiano\n' 
 global.dygp = 'https://chat.whatsapp.com/BOLb0ICN3sAJ5dloRBw5VD'
 global.fgsc = 'https://github.com/AbhishekSuresh2/ABHISHEK-SER' 
 global.fgyt = 'https://youtube.com/@comedymelodych8468'
